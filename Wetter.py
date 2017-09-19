a40630d1e28fa235cece4b2e45a0d3e9f0ce9484
Samuel <samuel.stenberg@hotmail.com>
2017-09-19 13:28:48 +0200

    fixed pdb format and sum vectors. Added some comments

--- Wetter.py
+++ Wetter.py
@@ -9,6 +9,9 @@ To-do list:
 . remove low (N < Nmax-1) coordinated atoms
 
 '''
+
+# pymatgen
+# ase
 import numpy as np
 import pandas as pd
 from radish import Topologizer
@@ -44,7 +47,7 @@ class Wetter:
 		H2 = [r*np.sin(-self.theta/2), 0, r*np.cos(-self.theta/2)]
 
 		#Append atoms to .pdb file
-	def appendAtoms(self, atomList = [], element='O', coords=[[0,0,0],[1,1,1]]):
+	def appendAtoms(self, atomList = [], element='H', coords=[[0,0,0],[1,1,1]]):
 		f = open(self.file, "r")
 		content = f.readlines()
 		f.close()
@@ -55,25 +58,109 @@ class Wetter:
 		#get number of atoms
 		nrAtoms = self.topol.trj.top.n_atoms
 
+		#Variables for the .pdb format
+		residueName = 'TiO'
+		chainIdentifier = 'A'
+		residueSequenceNumber = '1'
+		occupancy = '1.00'
+		temperatureFactor = '0.00'
+
 		#Prepare list of atoms to be appended to pdb file
 		for coord in coords:
-			atomList.append("ATOM   "+str(nrAtoms+1)+"  "+element+"   TiO A   1      "+self.float_format(coord[0])+"   "+self.float_format(coord[1])+"   "+self.float_format(coord[2])+"   1.00  0.00          "+element)
+			
+			#nrAtoms += 1
+			i = 4
 			nrAtoms += 1
+			tempString = "ATOM"
+
+			#Format tempString to .pdb format
+			while(i <= 80):
+				if(i + len(str(nrAtoms)) == 11):
+					tempString += str(nrAtoms)
+					i += len(str(nrAtoms))
+
+				elif(i + len(element) == 14):
+					tempString += element
+					i += len(element)
+
+				elif(i + len(residueName) == 20):
+					tempString += residueName
+					i += len(residueName)
+
+				elif(i +len(chainIdentifier) == 22):
+					tempString += chainIdentifier
+					i += 1
+
+				elif(i + len(residueSequenceNumber) == 26):
+					tempString += residueSequenceNumber
+					i += len(residueSequenceNumber)
+
+				elif(i + len(str(self.float_format(coord[0]))) == 38):
+					tempString += self.float_format(coord[0])
+					i += len(str(self.float_format(coord[0])))
+
+				elif(i + len(str(self.float_format(coord[1]))) == 46):
+					tempString += self.float_format(coord[1])
+					i += len(str(self.float_format(coord[1])))
+
+				elif(i + len(str(self.float_format(coord[2]))) == 54):
+					tempString += self.float_format(coord[2])
+					i += len(str(self.float_format(coord[2])))
+
+				elif(i + len(occupancy) == 60):
+					tempString += occupancy
+					i += len(occupancy)
+
+				elif(i + len(temperatureFactor) == 66):
+					tempString += temperatureFactor
+					i += len(temperatureFactor)
+
+				elif(i == 76):
+					tempString += element
+					i += len(element)
+
+				tempString += " "
+				i += 1
+
+			#Append formatted tempString
+			atomList.append(tempString)
+
+##################################    .pdb format     ##########################################
 	   	#"ATOM      1 Ti   TiO A   1       0.000   0.000  36.728  1.00  0.00          Ti"
 
+	   	'''
+			 1 -  6        Record name   "ATOM  "
+			 7 - 11        Integer       serial       Atom  serial number.
+			13 - 16        Atom          name         Atom name.
+			17             Character     altLoc       Alternate location indicator.
+			18 - 20        Residue name  resName      Residue name.
+			22             Character     chainID      Chain identifier.
+			23 - 26        Integer       resSeq       Residue sequence number.
+			27             AChar         iCode        Code for insertion of residues.
+			31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
+			39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
+			47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
+			55 - 60        Real(6.2)     occupancy    Occupancy.
+			61 - 66        Real(6.2)     tempFactor   Temperature  factor.
+			77 - 78        LString(2)    element      Element symbol, right-justified.
+			79 - 80        LString(2)    charge       Charge  on the atom.
+	   	'''
+#################################################################################################
+
+		#Don't know what to do with this yet.......
 	   	if(content[indices[-1] + 1][:3] == 'TER'):
 	   		print 'found'
 
 	   	new_content = content[:indices[-1] + 1]
-	   	
 	   	#Append new atoms
 	   	new_content.extend(atomList)
 	   	new_content.extend(content[indices[-1] + 1:])
 
 	   	#Print to file
-
-	   	#for line in new_content:
-	   	#	print line
+		file = open('test.pdb', 'w')
+		for line in new_content:
+			file.write("%s\n" % line.rstrip())	#also remove newline characters
+		file.close()
 
 	#Calculate M-O vectors
 	def calculateVectors(self):
@@ -83,33 +170,42 @@ class Wetter:
 			print("neighbourgraph:")
 			print self.neighbourgraph
 
-		#Initialize array to hold vectors
-		vectors = np.empty([0, 4], dtype=float)	#change initializer....
-		coords = np.array([[-1, -1, -1]])
+		vectors = np.empty([0, 3], dtype=float)
+		coords = np.empty([0, 3], dtype=float)
+		vec = np.array([0,0,0])
 
 		#Calculate M-O vectors
 		for center in tqdm(self.ind, ascii=True, desc='Progress'):
-			#Only calculate for the fraction that will actually get hydrated
-			if(np.random.rand(1) < self.frac):
-				for neighbour in self.neighbourgraph[center]:
-					#M-O Vector components
-					vec_x = self.topol.trj.xyz[0][center][0] - self.topol.trj.xyz[0][neighbour][0]
-					vec_y = self.topol.trj.xyz[0][center][1] - self.topol.trj.xyz[0][neighbour][1]
-					vec_z = self.topol.trj.xyz[0][center][2] - self.topol.trj.xyz[0][neighbour][2]
+			#Only calculate for the fraction that will actually get hydrated (change this later to actually choose that fraction)
+			vec = [0, 0, 0]
+			for neighbour in self.neighbourgraph[center]:
+				#M-O Vector components
+				vec_x = self.topol.trj.xyz[0][center][0] - self.topol.trj.xyz[0][neighbour][0]
+				vec_y = self.topol.trj.xyz[0][center][1] - self.topol.trj.xyz[0][neighbour][1]
+				vec_z = self.topol.trj.xyz[0][center][2] - self.topol.trj.xyz[0][neighbour][2]
 
-					#normalize. also save center to keep track of which atom the vector belongs to
-					mag = np.sqrt(vec_x**2 + vec_y**2 + vec_z**2)
-					vec = np.array([center, vec_x/mag, vec_y/mag, vec_z/mag])
+				#M-O vector
+				tempVec = np.array([vec_x, vec_y, vec_z])
 
-					#Coordinates where oxygen should be placed: vec + coordinate of metal atom
-					coord = np.array([self.topol.trj.xyz[0][center][0] + vec[1],
-										self.topol.trj.xyz[0][center][1] + vec[2],
-										self.topol.trj.xyz[0][center][2] + vec[3]])
-					
-					#Save coords and correct units
-					coords = np.vstack((coords, coord*10))
-					vectors = np.vstack((vectors, vec))
+				#Sum vectors
+				vec = [vec[0] + tempVec[0], vec[1] + tempVec[1], vec[2] + tempVec[2]]
 
+			#normalize
+			mag = np.sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
+			vec = [vec[0]/mag, vec[1]/mag, vec[2]/mag]
+
+			#Coordinates where oxygen should be placed: vec + coordinate of metal atom multiplied with bondlength
+			coord = np.array([self.topol.trj.xyz[0][center][0] + vec[0],
+								self.topol.trj.xyz[0][center][1] + vec[1],
+								self.topol.trj.xyz[0][center][2] + vec[2]])
+			
+			#print(coord)
+
+			#Save coords and correct units
+			coords = np.vstack((coords, coord*10))
+			vectors = np.vstack((vectors, vec))
+
+		print("Added " + str(len(coords)) + " atoms")
 		return vectors, coords
 
 
