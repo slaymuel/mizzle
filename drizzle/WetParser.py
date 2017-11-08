5dd1766d05a9818bad29a9b13232f4076aef2e35
Erik G. Brandt <erik.brandt@mmk.su.se>
2017-11-08 10:00:22 +0100

    Cleaned up parts of the code

--- drizzle/WetParser.py
+++ drizzle/WetParser.py
@@ -1,50 +1,53 @@
-"""Parses Wetter config file and finds Nmax
+"""Parses the configuration file and extract the maximum coordination for the metals
 
 """
 
 def parse(file):
-    """Parses config file
-    
+    """Parses the configuration in *file*
+
     """
 
-    f = open(file, 'r')
+    f = open(file, "r")
     content = f.readlines()
     f.close()
 
-    keyWords = ['atom', 'molecule', 'resname', 'end']
-    subKeyWords = ['defect', 'surface', 'HOH bond length', 'OH bond length', 'MOH bond length', 'bond angle', 'water', 'hydroxyl', 'fraction']
+    keyWords = ["atom", "molecule", "resname", "end"]
+    subKeyWords = ["defect", "surface", "HOH bond length",
+                   "OH bond length", "MOH bond length",
+                   "bond angle", "water", "hydroxyl", "fraction"]
     atoms = []
     resname = None
     i = 0
 
     while(i < len(content)):
-        if('atom' in content[i]):
-            keyWordIndex = content[i].index('atom')
-            colonIndex = content[i].index(':')
+        if("atom" in content[i]):
+            keyWordIndex = content[i].index("atom")
+            colonIndex = content[i].index(":")
             element = content[i][keyWordIndex + 5:colonIndex]
-            tempDict = {'element': element}
+            tempDict = {"element": element}
             coordination = content[i][colonIndex+2:].rstrip()
-            tempDict['coordination'] = coordination
+            tempDict["coordination"] = coordination
 
             i += 1
 
             while(not any(substring in content[i] for substring in keyWords)):
                 for subKeyWord in subKeyWords:
                     if(subKeyWord in content[i]):
-                        colonIndex = content[i].index(':')
+                        colonIndex = content[i].index(":")
                         tempDict[subKeyWord] = content[i][colonIndex + 2:].rstrip()
                 i += 1
             atoms.append(tempDict)
 
             continue
 
-        elif('resname' in content[i]):
-            colonIndex = content[i].index(':')
+        elif("resname" in content[i]):
+            colonIndex = content[i].index(":")
             resname = content[i][colonIndex+1:].strip()
             if(len(resname) > 3):
-                raise LookupError("Error in config file: resname can be no longer than 3 characters")
+                raise LookupError(("Error in config file: resname is restricted to "
+                                   "3 characters"))
 
-        elif(content[i].strip() == 'end'):
+        elif(content[i].strip() == "end"):
             break
 
         i += 1
@@ -68,10 +71,10 @@ def get_max_coordination(element):
     f.close()
     for line in content:
         if(element in line):
-            colonIndex = line.index(':')
+            colonIndex = line.index(":")
             maxCoordination = int(line[colonIndex + 1:])
             foundMax = True
             return maxCoordination
     if(not foundMax):
         raise ValueError(("Could not find maximum coordination number for "
-                          " {} element in {}.".format(element, maxNf)))
+                          "element {} in {}.".format(element, maxNf)))
